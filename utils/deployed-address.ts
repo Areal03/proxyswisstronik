1b7629ef2cae3549bed0a83c4a2e3414349288de
Real zxzx <arielyt03@gmail.com>
2024-07-31 19:43:12 +0000

    initial commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x0Fa987e1b6B713aCfEF3520B79759A64E5dFCb3A'
+export const ProxyAdmin = '0x7D93d09694099be22de58599F3f0369Bb2b5F457'
+export const SWTRImplementation = '0x16d4841933e3F4925195CD9357a45Ebe632f2f9b'
+    
